a190321257284aa896949c6ad08353a9944e38a6
Betrayer <StarDestroyer@ukr.net>
2020-03-19 18:47:58 +0200

    Updates

---
+++ precache-manifest.137f44a313fbde05b884eb91a7eea6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545f91bbf3c99d03d7b83232612bdeb6",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c7ca2d8f3d160a6b951b",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6d048306.chunk.css"
+  },
+  {
+    "revision": "f48ae523017e45544a48",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3928656d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3928656d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ca2d8f3d160a6b951b",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.4f8715a2.chunk.js"
+  },
+  {
+    "revision": "4992c48e99877600891f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ad90512f.js"
+  }
+]);
